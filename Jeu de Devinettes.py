6e64f44611594118f1cf547ccffc484af7b516ac
moormanx <moormanx@educ.college_montreal.qc.ca>
2023-10-12 16:20:34 -0400

    zshdtjej

--- Jeu de Devinettes.py
+++ Jeu de Devinettes.py
@@ -2,37 +2,46 @@
 # Créé par Xavier Moorman 2023
 
 import random
-
-nbr1 = int(input("Le code va choisir un nombre au hasard entre a et b. Donnez-moi un nombre, il va être utilisé comme nombre minimum ou 'a': "))
-nbr2 = int(input("Donnez-moi aussi votre nombre maximum ou 'b': "))
-
-nbr_random = random.randint(nbr1, nbr2)
-print(nbr_random)
-nbr_essais = 0
+rejouer = True
 
 
+while rejouer:
+    nbr1 = int(input("Donnez-moi un nombre, il va être utilisé comme nombre minimum ou 'a': "))
+    nbr2 = int(input("Donnez-moi aussi votre nombre maximum ou 'b': "))
 
-def loop1():
-    global nbr_essais
-    question = int(input("Essayez de deviner le nombre choisi entre le nombre 'a' et 'b' par le programme: "))
-    if question > nbr_random:
-        print("Le nombre est trop grand!")
-        nbr_essais= nbr_essais+1
-        loop1()
-    elif question < nbr_random:
-        print("Le nombre est trop petit!")
-        nbr_essais= nbr_essais+1
-        loop1()
-    else:
-        nbr_essais= nbr_essais+1
-        print("Vous avez réussi en",nbr_essais,"essais!")
-        rejouer = str(input("Voulez-vous rejouer? 'Y' ou 'N': "))
-        if rejouer == "Y":
-                loop1()
-                nbr_essais=0
-        elif rejouer == "N":
-            exit()
+    nbr_random = random.randint(nbr1, nbr2)
+    print(nbr_random)
+    reponse = True
+
+
+    nbr_essais = 0
+
+    while reponse:
+
+        question = int(input("Essayez de deviner le nombre choisi entre le nombre 'a' et 'b' par le programme: "))
+
+        if question > nbr_random:
+            print("Le nombre est trop grand!")
+            nbr_essais = nbr_essais + 1
+
+        elif question < nbr_random:
+            print("Le nombre est trop petit!")
+            nbr_essais = nbr_essais + 1
+
         else:
-            exit()
+            nbr_essais = nbr_essais + 1
+            print("Vous avez réussi en", nbr_essais, "essais!")
+            rejouerQ = str(input("Voulez-vous rejouer? 'Y' ou 'N': "))
+            if rejouerQ == "Y":
+                print("\n\n")
+                rejouer = True
 
-loop1()
+
+                nbr_essais = 0
+            elif rejouerQ == "N":
+                exit()
+                rejouer = False
+
+            else:
+                exit()
+
