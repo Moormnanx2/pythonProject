506b7a6cb5933a8531642dd2266619ede134ad8a
moormanx <moormanx@educ.college_montreal.qc.ca>
2023-10-20 10:37:13 -0400

    TP2 essai 1

--- Jeu de Devinettes.py
+++ Jeu de Devinettes.py
@@ -6,12 +6,12 @@ rejouer = True
 
 
 while rejouer:
-    nbr1 = int(input("Donnez-moi un nombre, il va être utilisé comme nombre minimum ou 'a': "))
-    nbr2 = int(input("Donnez-moi aussi votre nombre maximum ou 'b': "))
+    nbr1 = int(input("Donnez-moi un nombre, il va être utilisé comme nombre minimum ou 'a': ")) #Nbr plus petit choisi
+    nbr2 = int(input("Donnez-moi aussi votre nombre maximum ou 'b': ")) #Nbr plus grand choisi
 
-    nbr_random = random.randint(nbr1, nbr2)
-    print(nbr_random)
-    reponse = True
+    nbr_random = random.randint(nbr1, nbr2) #Choisir un nombre random dans les 2 nbr choisis au-dessus
+    #print(nbr_random) #Print le nbr random choisi pour régler des bogues et permettre d'en trouver plus facilement
+    reponse = True #Continue de faire le while de reponse, car c'est toujours vrai
 
 
     nbr_essais = 0
@@ -34,14 +34,14 @@ while rejouer:
             rejouerQ = str(input("Voulez-vous rejouer? 'Y' ou 'N': "))
             if rejouerQ == "Y":
                 print("\n\n")
-                reponse = False
+                reponse = False #Arrête le deuxième while, donc ça permet au premier de recommencer
 
 
                 nbr_essais = 0
             elif rejouerQ == "N":
                 exit()
-                rejouer = False
+                rejouer = False #Arrête le code entièrement
 
             else:
                 exit()
-                rejouer = False
+                rejouer = False #Arrête le code entièrement aussi
