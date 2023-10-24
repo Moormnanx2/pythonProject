9a7a11a1436fd4c2b3d9efc92467b494b7533b41
moormanx <moormanx@educ.college_montreal.qc.ca>
2023-10-24 11:54:54 -0400

    TP1 essai 2

--- word_count.py
+++ word_count.py
@@ -1,9 +1,10 @@
 # Made by Xavier Moorman en 2023, aidé par Alexandre Wilbur et Mathéo Bui
 
 
-def count_word(phrase=str(input("Donnez-moi votre phrase!: "))): #Fonction permet d'être appelée n'importe où
-    word_count = len(phrase .split())
+def count_word(phrase): #Fonction permet d'être appelée n'importe où et la phrase peut être changée
+    word_count = len(phrase .split()) #Trouve le nombre de mots dans une certaine phrase
     return(word_count) #Donne le nombre de mots de la phrase
 
-#hello = count_word() #Tester la fonction
-#print(hello) #Print le nombre de mots
+
+
+
